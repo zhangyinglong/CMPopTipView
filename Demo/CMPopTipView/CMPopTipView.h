97f5db27feb532dcf24a1abc680bac7f56bcd6a3
yinglong.zhang <yinglong.zhang@funplus.com>
2017-04-24 16:37:38 +0800

    surport carthage
    surport carthage import

---
+++ Demo/CMPopTipView/CMPopTipView.h
@@ -0,0 +1,19 @@
+//
+//  CMPopTipView.h
+//  CMPopTipView
+//
+//  Created by zhang yinglong on 2017/4/24.
+//  Copyright © 2017年 Chris Miles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CMPopTipView.
+FOUNDATION_EXPORT double CMPopTipViewVersionNumber;
+
+//! Project version string for CMPopTipView.
+FOUNDATION_EXPORT const unsigned char CMPopTipViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CMPopTipView/PublicHeader.h>
+
+
